73f8edd8be944a4a74093f2d08a334d2d76cceb4
lwy1471 <lwy1471@gmail.com>
2018-11-02 17:51:23 +0900

    11/2, developing APIs front pages.

--- run.py
+++ run.py
@@ -2,10 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/apis/')
 def hello():
-	return render_template('index.html')
-	
+	return render_template('apis.html')
+
+@app.route('/apis/assistant')
+def assistant():
+    return render_template('assistant.html')
 	
 if __name__ == '__main__':
 	app.run(debug=True)
