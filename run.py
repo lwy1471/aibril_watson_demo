81ceeec3d394a229d18777a721f0abb8046ec51a
lwy1471 <lwy1471@gmail.com>
2018-11-05 17:52:06 +0900

    modify Service Credential Session

--- run.py
+++ run.py
@@ -6,9 +6,14 @@ app = Flask(__name__)
 def hello():
 	return render_template('apis.html')
 
-@app.route('/apis/assistant')
+@app.route('/apis/assistant_old')
 def assistant():
     return render_template('assistant.html')
+
+@app.route('/apis/assistant')
+def assistant_test():
+    return render_template('assistant_test.html')
+
 	
 if __name__ == '__main__':
 	app.run(debug=True)
